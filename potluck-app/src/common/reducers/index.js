7d5e8818d3e7ba160d60179be3137f44c5c095eb
Gabe Chilson <g.chilson34@gmail.com>
2021-11-17 20:17:21 -0700

    deleted front-end folders

--- potluck-app/src/common/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import eventReducer from './eventReducer';
-
-const rootReducer = combineReducers({
-  eventReducer: eventReducer
-})
-
-export default rootReducer
